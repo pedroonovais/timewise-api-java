b9d3f9fddb867a0eb3ad559c86ed3611fc7c92b6
Pedro Novais <phenrique101007@gmail.com>
2025-11-11 20:48:29 -0300

    commit inicial

---
+++ src/main/java/com/timewise/timewise/TimewiseApplication.java
@@ -0,0 +1,13 @@
+package com.timewise.timewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimewiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timewise/timewise/TimewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timewise.timewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
